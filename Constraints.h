747b57ad0d4d9e2298f0496124f5dabf25971637
Remi-C <Remi-C@fake.fake>
2014-09-01 17:19:19 +0200

    adding weight and confidence in distance evaluation

--- Constraints.h
+++ Constraints.h
@@ -18,8 +18,8 @@ extern const double K_spacing;
   */
 struct DistanceToProjectionResidual {
    //! this is the constructor, it expects an array of at least 3 doubles.
-    DistanceToProjectionResidual(const double* input_vect,const double * input_w)
-        :position_(input_vect), w_i_j_(input_w) {}
+    DistanceToProjectionResidual(const double* input_vect,const double * input_w, const observation * input_obs)
+        :position_(input_vect), w_i_j_(input_w), obs_(input_obs) {}
 
     //! this is the operator computing the cost, ie the distance projeted on normal of (n_i,n_j)
         template <typename T> bool operator()(const T* const n_i,/**< the first node */
@@ -34,13 +34,16 @@ struct DistanceToProjectionResidual {
         soustraction(n_i,n_j,n_i_minus_n_j);
         T cross[3];
         ceres::CrossProduct(n_i_minus_obs,n_i_minus_n_j,cross);
-        distance_to_original_spacing[0] =   squaredNorm(cross)/squaredNorm(n_i_minus_n_j) -T(w_i_j_[0]) ;
+        distance_to_original_spacing[0] =
+                    T(K_obs) * T(obs_->confidence) * T(obs_->weight) *
+                (squaredNorm(cross)/squaredNorm(n_i_minus_n_j) -T(w_i_j_[0])) ;
 
         return true;
       }
  private:
     const double* position_; /**< store the 3D position of the observation point */
     const double* w_i_j_;//! the width of the edge, in meter
+    const observation * obs_;//link to the observation, for easy use of confidence and weight.
 };
 
 
@@ -60,7 +63,7 @@ struct DistanceToInitialPosition {
     s[0] =n_i[0]-T(initial_position_[0]);
     s[1] =n_i[1]-T(initial_position_[1]);
     s[2] =n_i[2]-T(initial_position_[2]);
-    distance_to_origin[0] = K_origin *  ceres::sqrt(s[0]*s[0] + s[1]*s[1] + s[2]*s[2] + 0.000001);
+    distance_to_origin[0] = T(K_origin) *  ceres::sqrt(s[0]*s[0] + s[1]*s[1] + s[2]*s[2] + 0.000001);
 
     return true;
   }
@@ -83,9 +86,9 @@ struct DistanceToInitialSpacing{
                                         T* distance_to_original_spacing) const {/**< this is the cost to be optimised*/
 
         //compute the difference with original spacing:
-        distance_to_original_spacing[0] = K_spacing * T(initial_spacing_[0]) - (n_i[0] - n_j[0])  ;
-        distance_to_original_spacing[1] = K_spacing * T(initial_spacing_[1]) - (n_i[1] - n_j[1])  ;
-        distance_to_original_spacing[2] = K_spacing * T(initial_spacing_[2]) - (n_i[2] - n_j[2])  ;
+        distance_to_original_spacing[0] = T(K_spacing) * T(initial_spacing_[0]) - (n_i[0] - n_j[0])  ;
+        distance_to_original_spacing[1] = T(K_spacing) * T(initial_spacing_[1]) - (n_i[1] - n_j[1])  ;
+        distance_to_original_spacing[2] = T(K_spacing) * T(initial_spacing_[2]) - (n_i[2] - n_j[2])  ;
 
         return true;
       }
