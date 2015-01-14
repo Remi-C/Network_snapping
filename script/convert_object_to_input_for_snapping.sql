66e684c27db69765a4f4984a3272bfffa69003d7
Remi-C <remi.Cura@gmail.com>
2015-01-14 17:50:56 +0100

    major rewrite of the query : way way faster !

--- script/convert_object_to_input_for_snapping.sql
+++ script/convert_object_to_input_for_snapping.sql
@@ -20,8 +20,8 @@ SET search_path TO objects_for_snapping,network_for_snapping, bdtopo_topological
  
   /* --this part are only to be done one time
 	--import table with objects :
-	DROP MATERIALIZED IF EXISTS ashaped_objects ; 
-	CREATE MATERIALIZED VIEW  ashaped_objects AS
+	DROP TABLE IF EXISTS ashaped_objects ; 
+	CREATE TABLE ashaped_objects AS
 		SELECT *
 		FROM public.dblink(
 		'hostaddr=127.0.0.1 port=5433 dbname=TerraMobilita user=postgres password=postgres'::text
@@ -47,15 +47,14 @@ SET search_path TO objects_for_snapping,network_for_snapping, bdtopo_topological
 		CREATE INDEX ON ashaped_objects  (should_be_used) ; 
 		CREATE INDEX ON ashaped_objects  (gid) ; 
 		--ANALYZE ashaped_objects;
-		--REFRESH MATERIALIZED VIEW ashaped_objects ;
-gid ,  classification_id ,  classification ,   z_range numrange, geom ,   import_error  , weighted_confidence ,   should_be_used 
+		--REFRESH MATERIALIZED VIEW ashaped_objects ; 
  
 	--mapping the object to the network 
 	
 
-	REFRESH MATERIALIZED VIEW ashaped_objects ;
+	--REFRESH MATERIALIZED VIEW ashaped_objects ;
 */
-	
+/*
 	DROP TABLE IF EXISTS obj_for_output_in_export_area; 
 	CREATE TABLE obj_for_output_in_export_area AS 
 	WITH obj_in_area AS ( --these are the observations that are inside the defined area
@@ -88,7 +87,7 @@ gid ,  classification_id ,  classification ,   z_range numrange, geom ,   import
 -- 					,'potted plant' 
 -- 					])
 -- 			UNION ALL
-				SELECT ao.gid ,  classification_id ,  classification 
+			SELECT ao.gid ,  classification_id ,  classification 
 					,  ST_SnapToGrid(ST_SimplifyPreserveTopology(ST_Buffer(ST_Buffer(ao.geom ,2,'quad_segs=4'),-2, 'quad_segs=4'),0.1),0.001) AS geom
 					, weighted_confidence ,   should_be_used 
 			FROM ashaped_objects AS  ao, def_zone_export as dfz
@@ -111,6 +110,11 @@ gid ,  classification_id ,  classification ,   z_range numrange, geom ,   import
 		FROM obj_in_area  AS oia ,edge_geom AS eg
 		WHERE ST_DWithin( ST_Transform(oia.geom,932011), eg.edge_geom,4+width)=TRUE
 			--order by usefull if using distinct
+				AND NOT EXISTS (
+				SELECT 1
+				FROM street_amp.result_intersection as ri
+				WHERE ST_DWithin(ri.intersection_surface,ST_Transform(oia.geom,932011),10)=TRUE
+			)
 		ORDER BY oia.gid ASC, abs(ST_Distance(ST_Transform(oia.geom,932011),ST_Buffer(eg.edge_geom,width/2,'endcap=flat' ))) ASC 
 	)
 	--object_id;class_id;class_name;edge_id;geom;confidence;
@@ -124,10 +128,59 @@ gid ,  classification_id ,  classification ,   z_range numrange, geom ,   import
 		, ST_Force2D(sline_to_edge) AS sline_to_edge
 		,weighted_confidence AS confidence 
 	FROM map  ; 
-
+*/
 
  
 	
+
+--trying to accelerate the querry : it doesn't scale ! 
+
+	DROP TABLE IF EXISTS obj_for_output_in_export_area; 
+	CREATE TABLE obj_for_output_in_export_area AS   
+	WITH crude_mapping AS ( --it is very counter intuitive, but it isway faster to force the data base to use the geometric filtering on inner product first, then use other
+		SELECT   oia.gid
+			,oia.classification
+			,oia.z_range
+			,oia.classification_id
+			,oia.weighted_confidence
+			,oia.geom
+			, eg.edge_id
+			, eg.width 
+			,eg.geom AS edge_geom
+		FROM ashaped_objects   AS oia, edges_for_output AS eg , def_zone_export as dfz
+		WHERE  
+		 ST_DWithin(  oia.geom , eg.geom,4+width) = true
+	)
+	,map AS (
+		SELECT DISTINCT ON (oia.gid) oia.*
+			,ST_ShortestLine( oia.geom  , edge_geom) as sline_to_edge
+		FROM crude_mapping as oia
+			INNER JOIN edges_for_output_in_export_area USING(edge_id) --enforce the fact that all edge are going to be exported
+			, def_zone_export as dfz 
+		WHERE classification = 'car'
+			AND (upper(z_range)-lower(z_range)) BETWEEN 0.2 AND 4 
+			AND ST_Area( oia.geom) BETWEEN 3.0 AND 12.0
+			AND  ST_WITHIN(oia.geom,  ST_Transform(dfz.geom ,932011) )= TRUE  --keeping only object that are in the export are 
+			AND NOT EXISTS ( --checking that object are not too close to intersection, or we don't consider theim
+				SELECT 1
+				FROM street_amp.result_intersection as ri
+				WHERE ST_DWithin(ri.intersection_surface, oia.geom ,10)=TRUE
+			)    
+		--order by usefull if using distinct
+		ORDER BY  oia.gid ASC, abs(ST_Distance(  oia.geom ,ST_Buffer(edge_geom,oia.width/2,'endcap=flat' ))) ASC  
+	)
+	--object_id;class_id;class_name;edge_id;geom;confidence;
+	SELECT  
+		row_number() over() AS qgis_id
+		,gid AS object_id 
+		,classification_id AS class_id
+		, classification AS class_name
+		,   edge_id 
+		, ST_Force2D(ST_SnapToGrid(ST_SimplifyPreserveTopology(ST_Buffer(ST_Buffer( geom ,2,'quad_segs=4'),-2, 'quad_segs=4'),0.1),0.001)  )  AS geom 
+		, ST_Force2D(sline_to_edge) AS sline_to_edge
+		,weighted_confidence AS confidence 
+	FROM map  ; 
+
  
 ---exporting the objects to file
 --#object_id;class_id;class_name;edge_id;geom;confidence;
@@ -157,3 +210,5 @@ COPY (
 	) AS sub
 )
 TO '/media/sf_E_RemiCura/PROJETS/snapping/data/data_in_reduced_export_area/object_in_export_area.csv';
+
+ 
